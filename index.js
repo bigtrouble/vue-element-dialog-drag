05922aebbd7b9d42f01a384fbfad6cc65d018b7b
huang_chengming <huang_chengming@linecorp.com>
2020-11-25 18:50:19 +0800

    #2 fixed

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
 "use strict";
-var vueElementDialogDraggable = {}
-vueElementDialogDraggable.install = function(Vue, options){
-	
-	Vue.directive('draggable', {
-		
-		bind : function(el, binding, vnode) {
+var D = {}
+D.install = function(Vue, options){
 
-			var dlg = el.getElementsByClassName("el-dialog")[0];
-			var title = el.getElementsByClassName("el-dialog__title")[0];
-			title.style.userSelect="none";
-			title.style["-ms-user-select"] = "none";
-			title.style["-moz-user-select"] = "none";
-			title.style.cursor="default";
+  const containment = options.containment ? true : false
 
-			dlg.offsetX = 0;
-			dlg.offsetY = 0;
-
-			var move = function(e){
-				dlg.style.marginLeft = '0px';
-				dlg.style.marginTop = '0px';
-				dlg.style.left = (e.pageX - dlg.offsetX) + 'px';
-				dlg.style.top = (e.pageY - dlg.offsetY) + 'px';
-			}
-
-			var up = function() {
-				removeEventListener('mousemove', move);
-				removeEventListener('mouseup', up);
-			}
-
-			var down = function(e){
-				dlg.offsetX = (e.pageX - dlg.offsetLeft);
-				dlg.offsetY = (e.pageY - dlg.offsetTop );
-
-				addEventListener('mousemove', move);
-				addEventListener('mouseup', up);
-			}
-
-			var header = el.getElementsByClassName("el-dialog__header")[0];
-			header.addEventListener('mousedown', down);
+  const getViewportSize = function() {
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+    return {
+      width, height
     }
-	});
+  }
+
+  const bind = function(el, binding, vnode) {
+    var dlg   = el.getElementsByClassName("el-dialog")[0]
+    var title = el.getElementsByClassName("el-dialog__title")[0]
+    title.style.userSelect="none"
+    title.style["-ms-user-select" ] = "none"
+    title.style["-moz-user-select"] = "none"
+    title.style.cursor="default"
+
+    dlg.offsetX = 0
+    dlg.offsetY = 0
+
+    const move = function(e){
+
+      var left = e.pageX - dlg.offsetX
+      var top  = e.pageY - dlg.offsetY
+      if(containment) {
+        //Constrains left & top
+        left = Math.max(left, 0)
+        top  = Math.max(top , 0)
+        //Constrains right & bottom
+        var viewport = getViewportSize()
+        left = Math.min(left, viewport.width  - dlg.offsetWidth)
+        top  = Math.min(top , viewport.height - dlg.offsetHeight)
+      }
+      dlg.style.margin     = '0px'
+      dlg.style.left       = left + 'px'
+      dlg.style.top        = top  + 'px'
+    }
+
+    const up = function() {
+      removeEventListener('mousemove', move)
+      removeEventListener('mouseup'  , up  )
+    }
+
+    const down = function(e){
+      dlg.offsetX = (e.pageX - dlg.offsetLeft)
+      dlg.offsetY = (e.pageY - dlg.offsetTop )
+      addEventListener('mousemove', move)
+      addEventListener('mouseup'  , up  )
+    }
+
+    const header = el.getElementsByClassName("el-dialog__header")[0]
+    header.addEventListener('mousedown', down)
+  }
+
+  Vue.directive('draggable', {
+    bind
+  })
 }
 
-module.exports = vueElementDialogDraggable;
+module.exports = D;
